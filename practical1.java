e3c197740e627f6f1b1d35c3f96a6a40c4ecf889
Shah-Namra <120650564+Shah-Namra@users.noreply.github.com>
2023-05-11 14:55:12 +0530

    Add files via upload

---
+++ practical1.java
@@ -0,0 +1,9 @@
+//Write a Program that displays Welcome to Java, Learning Java Now and Programming is fun.
+
+public class practical1 {
+    public static void main(String[] args){
+        System.out.print("Welcome to Java");
+        System.out.print("Learning Java Now");
+        System.out.print("Programming is Fun");
+    }
+}
